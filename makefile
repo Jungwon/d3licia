f4024f55fe83a68d824c952a17836c225ae7a4dd
kevcha <kevin.chavanne@gmail.com>
2012-10-02 18:15:58 +0200

    add makefile

---
+++ makefile
@@ -0,0 +1,22 @@
+JS_FILES = \
+	src/intro.js \
+	src/core.js \
+	src/outro.js
+
+JS_COMPILER = \
+	uglifyjs
+
+all: d3licia.js d3licia.min.js
+d3licia.js: $(JS_FILES)
+d3licia.min.js: $(JS_FILES)
+
+d3licia.js: Makefile
+	rm -f $@
+	cat $(filter %.js,$^) >> $@
+
+%.min.js:: Makefile
+	rm -f $@
+	cat $(filter %.js,$^) | $(JS_COMPILER) >> $@
+
+clean:
+	rm -rf d3licia.js d3licia.min.js
---
+++ src/core.js
@@ -0,0 +1,14 @@
+
+var d3licia = {
+	version: '0.0.1a',
+	dev: true
+};
+
+window.d3licia = d3licia;
+
+d3licia.tooltip = {}; // For the tooltip system
+d3licia.utils   = {}; // Utility subsystem
+d3licia.models  = {}; // stores all the possible models/components
+d3licia.charts  = {}; // stores all the ready to use charts
+d3licia.graphs  = []; // stores all the graphs currently on the page
+d3licia.logs    = {}; // stores some statistics and potential error messages
