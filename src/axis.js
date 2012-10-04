6d701c6e937efa314ba112698f46dabf4e2f0a7f
kevcha <kevin.chavanne@gmail.com>
2012-10-04 17:54:00 +0200

    add svg line rendering

--- src/axis.js
+++ src/axis.js
@@ -1,41 +1,49 @@
 
-d3licia.models.axis = function(data, scale) {
+d3licia.models.axis = function(options) {
 	// ===================================================
 	// Defaults values
-	// var defaults = {
-	// 	selector: '#graph',
-	// 	margin: 0
-	// };
+	var defaults = {
+		timestamp: false,
+		tickFormat: ',.0f',
+		timeFormat: ''
+	};
 	// ===================================================
 
 
 	// ===================================================
 	// Config values (= defaults + options) that will be used to draw chart
-	// var config = d3licia.utils.merge_options(defaults, options);
+	var config = d3licia.utils.merge_options(defaults, options);
 	// ===================================================
 
 
 	// ===================================================
-	// Config values (= defaults + options) that will be used to draw chart
-	//config.scale = d3licia.utils.merge_options(defaults, options);
+	// Format axis values for label if date
+	if (config.timestamp) {
+		var test = function(d) {
+			console.log(d);
+		}
+		console.log(config.scale.domain());
+
+			var formatTime = d3.time.format(config.timeFormat),
+			tickFormat = function(d) { return formatTime(new Date(2012, 0, 1, 0, d)); }; // TODO : Check this for other format than minutes
+	} else {
+		var tickFormat = d3.format(config.tickFormat);
+	}
 	// ===================================================
-var axis = d3.svg.axis()
-	.scale(scale)
-	.orient('bottom')
 
-	// vis.append("svg:path")
-	// 	.attr("class", "line")
-	// 	.attr("d", d3.svg.line()
-	// 	.x(function(d) { return x(d.x); })
-	// 	.y(function(d) { return y(d.y); }))
-	// 	.attr('fill', 'none')
 
-	// d3.select(config.selector)
-	// 	.append("svg:svg")
-	// 	.data([data])
-	// 	.attr("width", w + p * 2)
-	// 	.attr("height", h + p * 2)
-	// 	.append("svg:g")
-	// 	.attr("class", "test")
-	// 	.attr("transform", "translate(" + p + "," + p + ")")
+	var axis = d3.svg.axis()
+		.scale(config.scale)
+		.orient(config.orient)
+		.ticks(10)
+		.tickFormat(tickFormat);
+
+	return axis;
+
+
+	// Formatters for counts and times (converting numbers to Dates).
+	// var formatCount = d3.format(",.0f"),
+	// 		formatTime = d3.time.format("%H:%M"),
+	// 		formatMinutes = function(d) { return formatTime(new Date(2012, 0, 1, 0, d)); };
+
 };
